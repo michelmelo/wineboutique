876627cd99cd12ab625978ea07d96dd1a290be1c
Nikola Knežević <knezevicdev@gmail.com>
2018-12-03 15:01:25 +0100

    Register feedback

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,21 +14,51 @@
                 @csrf
                 <input type="hidden" name="type" value="CUSTOMER">
                 <input type="text" name="firstName" placeholder="First Name">
+                @if ($errors->has('firstName'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('firstName') }}</strong>
+                    </span>
+                @endif
                 <input type="text" name="lastName" placeholder="Last Name">
+                @if ($errors->has('lastName'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('lastName') }}</strong>
+                    </span>
+                @endif
                 <input type="email" name="email" placeholder="Email">
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
                 <input type="password" name="password" placeholder="Password">
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" name="acceptTerms" id="defaultCheck1">
                     <label class="form-check-label" for="defaultCheck1">
                         I agree to <a href="#">Terms and Conditions</a> of the Wine Boutique*
                     </label>
                 </div>
+                @if ($errors->has('acceptTerms'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('acceptTerms') }}</strong>
+                    </span>
+                @endif
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" name="acceptAge" id="defaultCheck2">
                     <label class="form-check-label" for="defaultCheck2">
                         I am at least 21 years of age.*
                     </label>
                 </div>
+                @if ($errors->has('acceptAge'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('acceptAge') }}</strong>
+                    </span>
+                @endif
                 <input type="submit" name="submit" class="button red-button full-width" value="CREATE AN ACCOUNT">
             </form>
         </div>
