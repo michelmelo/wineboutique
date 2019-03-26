ceb9839ac17c9b6ffa573c3e90a51d0c42c89b21
Nikola Knežević <knezevicdev@gmail.com>
2019-03-26 14:56:41 +0100

    My payment options

--- resources/views/my-address.blade.php
+++ resources/views/my-address.blade.php
@@ -8,7 +8,6 @@
         <div class="col-md-4 col-sm-12">
             <ul class="profile-menu">
                 <li class="active"><a href="profile"><i class="far fa-user"></i> Account Details</a></li>
-                <li><a href="#"><i class="far fa-credit-card"></i> My Payment Options</a></li>
                 <li><a href="my-address"><i class="fas fa-map-marker-alt"></i> My Address</a></li>
                 <li><a href="my-orders"><i class="far fa-list-alt"></i> My Orders</a></li>
                 <li><a href="my_favorites"><i class="far fa-heart"></i>My Favorites</a></li>
