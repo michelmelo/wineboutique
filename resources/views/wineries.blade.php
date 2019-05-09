c27e87999555ab08348a5015050a3c8b85215371
Arnold Ujhazi <mayonez89@gmail.com>
2019-05-09 19:59:24 +0200

    fixed prof and cover img upload (relocated), displayed on /winery/{winery} and /wineries

--- resources/views/wineries.blade.php
+++ resources/views/wineries.blade.php
@@ -12,8 +12,8 @@
             <div class="col-lg-6 wineries-box">
                 <div>
                     <div class="wineries-brand">
-                        <img class="winery-header" src="{{asset('img/winery-1.jpg')}}">
-                        <img class="winery-logo" src="{{asset('img/winery-logo-1.jpg')}}">
+                        <img class="winery-header" src="{{ $winery->cover==null ? asset('img/winery-1.jpg') : '/images/winery/cover/'.$winery->cover }}">
+                        <img class="winery-logo" src="{{ $winery->profile==null ? asset('img/winery-logo-1.jpg') : '/images/winery/profile/'.$winery->profile }}">
                     </div>
                     <p><a href="{{route('winery', $winery->slug)}}">{{$winery->name}}</a></p>
                 </div>
