399166efff25be00d9231725d3d3edf030b535a4
Dejan <dejan.jankov.20@gmail.com>
2019-06-10 17:13:52 +0200

    implementation of standard box desing for search results

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -7,26 +7,30 @@
         </div>
         <div class="row">
             @foreach ($results as $result)
-                <div class="col-md-3 col-sm-6 col-xs-6 vine-box-style-3 style-3-2">
+                <div class="col-xs-5 vine-box-style-3 style-3-2">
                     <a href="{{$result->type==='WINE'?route('wine.show', ['wine' => $result->slug]):route('winery', ['winery' => $result->slug])}}">
                         <div class="image-container">
                             <img src=" {{ $result->photo }}">
                             <div class="overlay"></div>
-                            <h5>{{$result->name}}</h5>
-                            <small>{{$result->description}}</small>
                             @if($result->type==='WINE')
                                 @if(Auth::user())
-                                    <favorite
-                                            :post="'{{ $result->slug }}'"
-                                            :favorited="{{ $result->favorited ? 'true' : 'false' }}"
-                                            :type="'wine'"
-                                    ></favorite>
+                                        <favorite
+                                                :post="'{{ $result->slug }}'"
+                                                :favorited="{{ $result->favorited ? 'true' : 'false' }}"
+                                                :type="'wine'"
+                                        ></favorite>
+                                    </div>
                                 @endif
-                                <star-rating :star-size="15" active-color="#991D3F" :show-rating="false" :read-only="true" :rating="{{$result->rating}}"></star-rating>
-                                <div class="wine-price">
-                                    ${{$result->price}}
+                                <div class="product-info">
+                                    <h5>{{$result->name}}</h5>
+                                    <h4 class="wine-price">
+                                        ${{$result->price}}
+                                    </h4>
+                                    <div class="star-rating">
+                                        <star-rating :star-size="15" active-color="#991D3F" :show-rating="false" :read-only="true" :rating="{{$result->rating}}"></star-rating>
+                                    </div>
+                                    <span class="order-q">{{$result->orders_count}} Orders</span>
                                 </div>
-                                <span class="order-q">{{$result->orders_count}} Orders</span>
                             @endif
                         </div>
                     </a>
