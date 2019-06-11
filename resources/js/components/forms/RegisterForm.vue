f28cc1949d4a44f72ff1b9547b797c737e17cab5
Petar Vukotic <petarb45@gmail.com>
2019-06-11 18:12:43 +0200

    validations

---
+++ resources/js/components/forms/RegisterForm.vue
@@ -0,0 +1,145 @@
+<template>
+<div>
+    <form class="form-inline" v-on:submit="onSubmit" method="post" action="/register">
+        <input type="hidden" name="_token" v-model="csrf">
+        <input type="hidden" name="type" value="CUSTOMER">
+        <input type="text" v-model.trim="firstName" :class="{ 'invalid': isInvalid('firstName') }" name="firstName" placeholder="First Name">
+        <span class="help-block" v-if="isInvalid('firstName')">
+            <strong>First Name is required.</strong>
+        </span>
+        <input type="text" v-model.trim="lastName" :class="{ 'invalid': isInvalid('lastName') }" name="lastName" placeholder="Last Name">
+        <span class="help-block" v-if="isInvalid('lastName')">
+            <strong>Last Name is required.</strong>
+        </span>
+        <input type="email" v-model.trim="email" :class="{ 'invalid': isInvalid('email') || validEmail===false, 'valid': validEmail }" name="email" placeholder="Email">
+        <span class="help-block" v-if="isInvalid('email')">
+            <strong>Email is invalid.</strong>
+        </span>
+        <input type="password" v-model="password" :class="{ 'invalid': isInvalid('password') }" name="password" placeholder="Password">
+        <span class="help-block" v-if="isInvalid('password')">
+            <strong>Password is invalid.</strong>
+        </span>
+        <div class="form-check">
+            <input class="form-check-input" v-model="acceptTerms" type="checkbox" name="acceptTerms" id="defaultCheck1">
+            <label class="form-check-label" :class="{ 'invalid': isInvalid('acceptTerms') }" for="defaultCheck1">
+                I agree to <a href="#">Terms and Conditions.</a> of the Wine Boutique*
+            </label>
+        </div>
+        <span class="help-block" v-if="isInvalid('acceptTerms')">
+            <strong>You must accept Terms and Conditions.</strong>
+        </span>
+        <div class="form-check">
+            <input class="form-check-input" v-model="acceptAge" type="checkbox" name="acceptAge" id="defaultCheck2">
+            <label class="form-check-label" :class="{ 'invalid': (isInvalid('acceptAge') || isInvalid('birthday')) }" for="defaultCheck2">
+                I am at least 21 years of age.*
+            </label>
+        </div>
+        <span class="help-block" v-if="isInvalid('acceptAge')">
+            <strong>You must be 21 or older.</strong>
+        </span>
+        <div id="over-21" v-if="acceptAge">
+            <input type="date" name="birthday" v-model="birthday"  :max="initialDate()"/>
+        </div>
+        <input type="submit" name="submit" class="button red-button full-width" value="CREATE A WINERY">
+    </form>
+</div>
+</template>
+
+<script>
+    import { required, minLength, email, numeric } from 'vuelidate/lib/validators';
+    import { isTrue } from "../../customValidators";
+    import moment from 'moment';
+
+    const formFields = [
+        'firstName',
+        'lastName',
+        'email',
+        'password',
+        'acceptTerms',
+        'acceptAge',
+        'birthday'
+    ];
+
+    export default {
+        data: () => ({
+
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            acceptTerms: false,
+            acceptAge: false,
+            birthday: '',
+            csrf: window.Laravel.csrfToken,
+            showErrors: false,
+            validEmail: null
+        }),
+        watch: {
+            'email': 'checkEmail'
+        },
+        methods: {
+            checkEmail() {
+                this.validEmail = null;
+                if(this.email.length > 0 && email(this.email)) {
+                    axios.post(`/api/auth/check-email`, {email: this.email})
+                        .then(() => {
+                            this.validEmail = true;
+                        })
+                        .catch(() => {
+                            this.validEmail = false;
+                        });
+                }
+            },
+            onSubmit(event) {
+                this.showErrors = true;
+                if(this.$v.$invalid) {
+                    event.preventDefault();
+                    formFields.some(formField => {
+                        if(this.$v[formField].$invalid) {
+                            document.querySelector(`[name="${formField}"]`).focus();
+                            return true;
+                        }
+                        return false;
+                    });
+                }
+            },
+            isInvalid(name) {
+                console.log(name, this.$v[name]);
+                return this.$v[name].$invalid && this.showErrors;
+            },
+            initialDate(){
+                let date = new Date();
+                date.setFullYear(date.getFullYear() - 21);
+
+                return moment(date).format('Y-MM-DD');
+            }
+        },
+        validations: {
+            firstName: {
+                required,
+                minLength: minLength(4)
+            },
+            lastName: {
+                required,
+                minLength: minLength(4)
+            },
+            email: {
+                required,
+                email
+            },
+            password: {
+                required,
+                minLength: minLength(6)
+            },
+            acceptTerms: {
+                isTrue
+            },
+            acceptAge: {
+                isTrue
+            },
+            birthday: {
+                required
+            }
+        }
+    }
+</script>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <div class="container">
-
     <div class="row padding-row">
         <h1 class="headline-2">CUSTOMER SIGN UP</h1>
         <div class="col-lg-6 col-md-12 mobile-display">
@@ -10,64 +9,8 @@
         </div>
 
         <div class="col-lg-6 col-md-12 forms">
-            <form class="form-inline" method="post" action="{{route('register')}}">
-                @csrf
-                <input type="hidden" name="type" value="CUSTOMER">
-                <input type="text" name="firstName" placeholder="First Name" required value="{{ old('firstName') ? old('firstName') : '' }}">
-                @if ($errors->has('firstName'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('firstName') }}</strong>
-                    </span>
-                @endif
-                <input type="text" name="lastName" placeholder="Last Name" required value="{{ old('lastName') ? old('lastName') : '' }}">
-                @if ($errors->has('lastName'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('lastName') }}</strong>
-                    </span>
-                @endif
-                <input type="email" name="email" placeholder="Email" required value="{{ old('email') ? old('email') : '' }}">
-                @if ($errors->has('email'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                @endif
-                <input type="password" name="password" placeholder="Password" required>
-                @if ($errors->has('password'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                @endif
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="acceptTerms" id="defaultCheck1" required>
-                    <label class="form-check-label" for="defaultCheck1">
-                        I agree to <a href="#">Terms and Conditions</a> of the Wine Boutique*
-                    </label>
-                </div>
-                @if ($errors->has('acceptTerms'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('acceptTerms') }}</strong>
-                    </span>
-                @endif
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="acceptAge" id="defaultCheck2" required>
-                    <label class="form-check-label" for="defaultCheck2">
-                        I am at least 21 years of age.*
-                    </label>
-                </div>
-                @if ($errors->has('acceptAge'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('acceptAge') }}</strong>
-                    </span>
-                @endif
-
-                <div id="over-21" class="d-none">
-                    <input type="date" name="birthday" max="{{ date("Y-m-d", strtotime("-21 year", time())) }}" required />
-                </div>
-
-                <input type="submit" name="submit" class="button red-button full-width" value="CREATE AN ACCOUNT">
-            </form>
+            <register-form />
         </div>
     </div>
-
 </div>
 @endsection
