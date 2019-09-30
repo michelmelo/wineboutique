296dc9a6e8d53fe4a7be6a9cf3454bcdd6c920eb
Petar Vukotic <petarb45@gmail.com>
2019-09-30 14:20:22 +0200

    fixes

--- resources/views/email/order-confirmation.blade.php
+++ resources/views/email/order-confirmation.blade.php
@@ -1,70 +1,12 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-</head>
-<body style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #f5f8fa; color: #74787E; height: 100%; hyphens: auto; line-height: 1.4; margin: 0; -moz-hyphens: auto; -ms-word-break: break-all; width: 100% !important; -webkit-hyphens: auto; -webkit-text-size-adjust: none; word-break: break-word;">
-<style>
-    @media  only screen and (max-width: 600px) {
-        .inner-body {
-            width: 100% !important;
-        }
-
-        .footer {
-            width: 100% !important;
-        }
-    }
-
-    @media  only screen and (max-width: 500px) {
-        .button {
-            width: 100% !important;
-        }
-    }
-</style>
-
-<table class="wrapper" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #f5f8fa; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
-    <tr>
-        <td align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-            <table class="content" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
-                <tr>
-                    <td class="header" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 25px 0; text-align: center; background-color: #fff;">
-                        <a href="http://localhost" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #fff; font-size: 19px; font-weight: bold; text-decoration: none; text-shadow: 0 1px 0 white;">
-                            <img src="http://stage.wineboutique.execdigi.com/img/logo.png" alt="wineBoutique-logo" />
-                        </a>
-                    </td>
-                </tr>
-
-                <!-- Email Body -->
-                <tr>
-                    <td class="body" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #fff; border-bottom: 1px solid #991338; border-top: 1px solid #991338; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
-                        <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #FFFFFF; margin: 0 auto; padding: 0; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
-                            <!-- Body content -->
-                            <tr>
-                                <td class="content-cell" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;background-color: #fff;">
-                                    <h1 style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #000000; font-size: 19px; font-weight: bold; margin-top: 0; text-align: left;">Order Confirmed!</h1>
-                                    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #000000; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">Thank you for your order!</p>
-                                    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #000000; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">It will be shipped in shortest possible time.</p>
-                                    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #000000; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">Cheers,<br>Wine Boutique</p>
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;background-color: #fff;">
-                        <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0 auto; padding: 0; text-align: center; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
-                            <tr>
-                                <td class="content-cell" align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
-                                    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; line-height: 1.5em; margin-top: 0; color: #AEAEAE; font-size: 12px; text-align: center;">© 2019 Wine Boutique. All rights reserved.</p>
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-</table>
-</body>
-</html>
+<div class="container">
+    <div class="page-header" style="text-align: center">
+        <img src="{{asset('img/wines-header.jpg')}}" style="width: 100%">
+    </div>
+    <h1>THANK YOU</h1>
+    <p style="margin-bottom: 30px;">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias praesentium,
+        mollitia culpa, itaque nobis quisquam architecto laboriosam fuga dignissimos quidem fugiat. Odio
+        consequuntur provident sequi ut culpa placeat temporibus cum? Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Maiores sint architecto, non corporis totam necessitatibus perspiciatis deleniti aperiam
+        magnam laudantium debitis harum provident animi! Libero, ut odit. Laboriosam, amet similique!
+    </p>
+</div>
