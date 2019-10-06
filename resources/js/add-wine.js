6e7370170bceb1876f2f3fb267d6b21f5d136c87
Petar Vukotic <petarb45@gmail.com>
2019-10-06 23:48:16 +0200

    crop other images temp commit

--- resources/js/add-wine.js
+++ resources/js/add-wine.js
@@ -68,64 +68,21 @@ $(document).ready(function() {
         $(".add-new-wine button#submit").prop("disabled", "disabled");
         $("#crop-error").show();
     });
+
+    $(".other_image").change(function(event){
+        var formData = new FormData();
+        formData.append('image', $(this)[0].files[0]);
+
+        $.ajax({
+            url : "/wine-image",
+            type : 'POST',
+            data : formData,
+            processData: false,
+            contentType: false,
+            success : function(response) {
+                $(`<input type="hidden" name="images[]" value="${response.id}" />`).appendTo("#inputs");
+                $(".other_images_preview").append('<img src="' + URL.createObjectURL(event.target.files[0]) + '">');
+            }
+        });
+    });
 });
-
-Dropzone.options.photos = {
-    url: "/wine-image",
-    paramName: "image",
-    maxFilesize: 10,
-    resizeMimeType: "image/jpeg",
-    acceptedFiles: "image/jpeg,image/jpg,image/png,image/gif,image/webp",
-    addRemoveLinks: true,
-    dictDefaultMessage: "Click here to add more images!",
-    accept: function(file, done) {
-        if (file.name == "justinbieber.jpg") {
-            done("Naha, you don't.");
-        }
-        else { document.getElementsByClassName('add-more-images')[0].style.visibility = "visible"; done(); }
-    },
-    sending: function(file, xhr, formData) {
-        formData.append("_token", document.querySelector("input[name='_token']").value);
-    },
-    success: function(file, response) {
-        $(file.previewElement).attr("data-id", response.id);
-        $(`<input type="hidden" name="images[]" value="${response.id}" />`).appendTo("#inputs");
-    },
-    removedfile: async function(file) {
-        const $filePreview = $(file.previewElement);
-        const id = $filePreview.data("id");
-        $filePreview.remove();
-        $(`[name="images[]"][value="${id}"]`).remove();
-        if($('[name="_method"][value="PUT"]').length) {
-            $(`<input type="hidden" name="delete_images[]" value="${id}" />`).appendTo("#inputs");
-        } else {
-            $.ajax({
-                url: `/wine-image/${id}`,
-                type: 'DELETE'
-            });
-        }
-    },
-    init: function() {
-        if(typeof preloadedImages !== "undefined") {
-            const thisDropzone = this;
-
-            preloadedImages.forEach(preloadedImage => {
-                document.getElementsByClassName('add-more-images')[0].style.visibility = "visible";
-                const mockFile = { 
-                    name: preloadedImage.path.replace(/^.*[\\\/]/, ''), 
-                    size: preloadedImage.size,
-                    status: Dropzone.ADDED,
-                    accepted: true
-                }; 
-
-                thisDropzone.emit("addedfile", mockFile);
-                thisDropzone.emit("thumbnail", mockFile, preloadedImage.path);
-                thisDropzone.emit("complete", mockFile);
-                thisDropzone.files.push(mockFile);
-                $(`img[src='${preloadedImage.path}']`)
-                    .closest(".dz-preview")
-                    .attr("data-id", preloadedImage.id);
-            });
-        }
-    }
-};
