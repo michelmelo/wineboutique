5e9527a21e72e1fd7762c1e06dbb2c43d3282e01
Petar Vukotic <petarb45@gmail.com>
2019-10-07 13:38:12 +0200

    crop image aspect racio

--- resources/js/add-wine.js
+++ resources/js/add-wine.js
@@ -88,7 +88,7 @@ function readURL(input, selector) {
 
         setTimeout(function () {
             selector.cropper({
-                aspectRatio: 0.9,
+                aspectRatio: 0.85,
                 rotatable: false,
                 scalable: false,
                 zoomable: false
