e77179d531be9fe62184ed00129de0ed35d83fa1
Vladislav Mosnak <vm@executive-digital.com>
2019-11-01 16:18:17 +0100

    Fix top rated wines on homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
         $topWineries = DB::select('SELECT *, IFNULL((SELECT AVG(`winery_ratings`.`rating`) FROM `winery_ratings` WHERE `winery_ratings`.`winery_id`=`wineries`.`id`), 0) AS `rating` FROM `wineries` HAVING `rating` > 0 ORDER BY `rating` DESC LIMIT 10');
         $latestWines = Wine::latest()->where("quantity", ">", 0)->limit(6)->get();
         
-        $wines = Wine::orderBy('average_rating', 'desc')->where("quantity", "<", 0);
+        $wines = Wine::orderBy('average_rating', 'desc')->where("quantity", ">", 0);
         $varietals = Varietal::all();
         $regions = Region::orderBy('name')->get();
 
