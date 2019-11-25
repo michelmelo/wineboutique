307e470e4b81ddcafbb5cb06ac31d1c09b060126
mayonez89 <mayonez89@gmail.com>
2019-11-25 21:19:01 +0200

    quickfix

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -115,7 +115,7 @@ class CheckoutController extends Controller
 
                         break;
                     }
-                    if($wine->quantity > Wine::where('id', $wine->id)->first()) {
+                    if($wine->quantity > Wine::where('id', $wine->id)->first()->quantity) {
                         return redirect()->back()->with('message', 'Not enough ' . $wine->name  . ' in the storage.');
                     }
                 }
