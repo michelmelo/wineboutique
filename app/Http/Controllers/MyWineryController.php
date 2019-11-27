a731e3150712771edb055111c4ce15e2fba1d0bb
mayonez89 <mayonez89@gmail.com>
2019-11-27 15:42:58 +0200

    minifix

--- app/Http/Controllers/MyWineryController.php
+++ app/Http/Controllers/MyWineryController.php
@@ -63,30 +63,33 @@ class MyWineryController extends Controller
             $do_save = true;
             $do_edit = !is_null($shippings["id"]);
 
-            foreach ($shippings as $key => $shipping){
-                if((is_null($shipping) && $key != "id") || (is_null($shipping) && $do_edit)){
+            foreach ($shippings as $key => $shipping) {
+                if ((is_null($shipping) && $key != "id" && $key != 'additional') || (is_null($shipping) && $do_edit)) {
                     $do_save = false;
                 }
             }
 
-            if($do_save){
-                if(isset($shippings['shipping_free'])){
+            if ($do_save) {
+                if (isset($shippings['shipping_free'])) {
                     $shippings['price'] = 0;
                     $shippings['additional'] = 0;
 
                     unset($shippings['shipping_free']);
                 }
 
-                if($do_edit){
-                    WineryShipping::where('id', $shippings["id"])->update($shippings);
+                if(!isset($shippings['additional'])||$shippings['additional']==null) {
+                    $shippings['additional'] = 0;
                 }
-                else{
-                    foreach ($shippings['ship_to'] as $to){
+
+                if ($do_edit) {
+                    WineryShipping::where('id', $shippings["id"])->update($shippings);
+                } else {
+                    foreach ($shippings['ship_to'] as $to) {
                         $winery->winery_shippings()->create([
                             "ship_from" => $shippings['ship_from'],
                             "ship_to" => $to,
                             "price" => $shippings['price'],
-                            "additional" => isset($shippings['additional']) && $shippings['additional'] !== null ? $shippings['additional'] : 0,
+                            "additional" => $shippings['additional'],
 //                            "days_from" => $shippings['days_from'],
 //                            "days_to" => $shippings['days_to']
                         ]);
