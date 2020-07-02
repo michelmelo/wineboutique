dac1096e98522f9e4194320a7ba544837f7d892f
mayonez89 <mayonez89@gmail.com>
2020-07-02 19:44:21 +0200

    temp

--- resources/js/components/forms/CheckoutForm.vue
+++ resources/js/components/forms/CheckoutForm.vue
@@ -165,7 +165,28 @@
                             <button class="button red-button full-width d-block w-100 payment-submit" type="submit" v-if="hasPayment && canMakePayment">
                                 Place order
                             </button>
-                            <a href="/my-payments" class="text-red font-weight-bold" v-if="!hasPayment">Add payment method please</a>
+                            <div v-if="!hasPayment" id="formReplacement">
+                                <div class="form-row">
+                                    <label for="card-element">
+                                        Credit or debit card
+                                    </label>
+                                    <div id="card-element">
+                                        <!-- A Stripe Element will be inserted here. -->
+                                    </div>
+
+                                    <!-- Used to display form errors. -->
+                                    <div id="card-errors" role="alert"></div>
+                                </div>
+                                <br>
+                                <div class="form-row">
+                                    <input type="text" name="alias" class="StripeElement" placeholder="Card alias" required>
+                                </div>
+                                <br>
+                                <div class="form-row">
+                                    <button type="button" id="sbmt-new-payment-mtd" class="sbmt-new-payment-mtd button red-button">Submit New Payment Method</button>
+                                </div>
+                            </div>
+                            <!--<a href="/my-payments" class="text-red font-weight-bold" v-if="!hasPayment">Add payment method please</a>-->
                         </div>
                     </article>
                 </div>
@@ -210,6 +231,10 @@
                     region_id: '',
                     default: true
                 },
+                ccInfo: {
+                    alias: '',
+
+                },
                 showErrors: false,
                 dateC: null,
             }
@@ -217,6 +242,7 @@
         mounted() {
             this.asyncData();
             this.dateC = (new Date()).toLocaleDateString();
+            this.mountCC();
         },
         methods: {
             checkIfCanPay: function(v) {
@@ -308,6 +334,82 @@
             newAddressFn(selectedAddress) {
                 this.addresses.push(selectedAddress)
                 this.selectedAddress = selectedAddress;
+            },
+            mountCC() {
+                var stripe = Stripe('pk_test_bWZc4BcEaCNAKbJbhv6u91ZJ00zZEQ2RIQ');
+                var elements = stripe.elements();
+
+                // Custom styling can be passed to options when creating an Element.
+                // (Note that this demo uses a wider set of styles than the guide below.)
+                var style = {
+                    base: {
+                        color: '#32325d',
+                        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+                        fontSmoothing: 'antialiased',
+                        fontSize: '16px',
+                        '::placeholder': {
+                            color: '#aab7c4'
+                        }
+                    },
+                    invalid: {
+                        color: '#fa755a',
+                        iconColor: '#fa755a'
+                    }
+                };
+
+                // Create an instance of the card Element.
+                var card = elements.create('card', {style: style});
+
+                // Add an instance of the card Element into the `card-element` <div>.
+                card.mount('#card-element');
+
+                // Handle real-time validation errors from the card Element.
+                card.addEventListener('change', function(event) {
+                    var displayError = document.getElementById('card-errors');
+                    if (event.error) {
+                        displayError.textContent = event.error.message;
+                    } else {
+                        displayError.textContent = '';
+                    }
+                });
+
+                // Handle form submission.
+                var btn = document.getElementById('sbmt-new-payment-mtd');
+                var wasSubmitted = false;
+
+                btn.addEventListener('click', function(event) {
+                    event.preventDefault();
+
+                    if(!wasSubmitted) {
+                        wasSubmitted = true;
+                        btn.disabled = true;
+
+                        stripe.createToken(card).then(function(result) {
+                            if (result.error) {
+                                // Inform the user if there was an error.
+                                var errorElement = document.getElementById('card-errors');
+                                errorElement.textContent = result.error.message;
+                            } else {
+                                // Send the token to your server.
+                                stripeTokenHandler(result.token);
+                            }
+                        });
+                    }
+                });
+                //
+                // Submit the form with the token ID.
+                function stripeTokenHandler(token) {
+                    // Ins  ert the token ID into the form so it gets submitted to the server
+                    var form = document.getElementById('payment-form');
+                    var hiddenInput = document.createElement('input');
+                    hiddenInput.setAttribute('type', 'hidden');
+                    hiddenInput.setAttribute('name', 'stripeToken');
+                    hiddenInput.setAttribute('value', token.id);
+                    form.appendChild(hiddenInput);
+
+                    // Submit the form
+                    form.submit();
+                }
             },
         },
         computed: {
