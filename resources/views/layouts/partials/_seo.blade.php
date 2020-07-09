c7c3dbd3545e76217f764111db54bb0a9129ae3e
mayonez89 <mayonez89@gmail.com>
2020-07-09 13:38:44 +0200

    add noindex

--- resources/views/layouts/partials/_seo.blade.php
+++ resources/views/layouts/partials/_seo.blade.php
@@ -1,15 +1,18 @@
 <title>{{ $seo['title'] ?? 'Wine Boutique' }}</title>
-<meta name="description" content="{{ $seo['description'] ?? 'Wine Boutique' }}">
-<link rel="canonical" href="{{ url('') }}" />
+@if(!empty(env('IS_INDEXING')) && env('IS_INDEXING') == 'no')
+    <meta name="robots" content="noindex"/>
+@endif
+    <meta name="description" content="{{ $seo['description'] ?? 'Wine Boutique' }}">
+    <link rel="canonical" href="{{ url('') }}" />
 
-<meta property="og:url" content="{{ Request::url() }}" />
-<meta property="og:title" content="{{ $seo['title'] ?? 'Wine Boutique' }}" />
-<meta property="og:description" content="{{ $seo['description'] ?? 'Wine Boutique' }}" />
-<meta property="og:image" content="{{ $seo['image'] ?? '' }}" />
-<meta property="og:type" content="website" />
-<meta property="og:site_name" content="Wine Boutique" />
+    <meta property="og:url" content="{{ Request::url() }}" />
+    <meta property="og:title" content="{{ $seo['title'] ?? 'Wine Boutique' }}" />
+    <meta property="og:description" content="{{ $seo['description'] ?? 'Wine Boutique' }}" />
+    <meta property="og:image" content="{{ $seo['image'] ?? '' }}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Wine Boutique" />
 
-<meta name="twitter:card" content="summary" />
-<meta name="twitter:title" content="{{ $seo['title'] ?? 'Wine Boutique' }}" />
-<meta name="twitter:description" content="{{ $seo['description'] ?? 'Wine Boutique' }}" />
-<meta name="twitter:image" content="{{ $seo['image'] ?? '' }}" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="{{ $seo['title'] ?? 'Wine Boutique' }}" />
+    <meta name="twitter:description" content="{{ $seo['description'] ?? 'Wine Boutique' }}" />
+    <meta name="twitter:image" content="{{ $seo['image'] ?? '' }}" />
